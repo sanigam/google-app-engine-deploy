62f47d8dba19341978434b4bcf9a435ee6305311
Sampann Nigam <sampann_nigam@yahoo.com>
2021-01-30 13:15:28 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from google.cloud import storage
 import numpy as np
 
 storage_client = storage.Client()
-bucket = storage_client.get_bucket('model-bucket-iris')
+bucket = storage_client.get_bucket('model-bucket-iris-sn')
 blob = bucket.blob('flower-v1.pkl')
 blob.download_to_filename('/tmp/flower-v1.pkl')
 model_pk = pickle.load(open('/tmp/flower-v1.pkl', 'rb'))
